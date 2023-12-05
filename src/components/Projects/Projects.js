f5d687ef4878e4b7c6d214429152516b3965f8c1
Abhijit Barman <113384779+Abhijit0Barman@users.noreply.github.com>
2023-12-06 01:54:14 +0530

    gg

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -15,7 +15,10 @@ import github from "../images/github.jpg";
 import redux from "../images/redux.jpg";
 import postman from "../images/postman.jpg";
 import styledmy from "../images/styledmy.jpg";
-
+import fastFashion from "../../assets/projects/fastFashion.png";
+import firebase from "../../assets/techStack/firebase.png";
+import stripe from "../../assets/techStack/stripe.png";
+import travelo from "../../assets/projects/travelo.png";
 
 /*
 const Works = () => {
@@ -43,110 +46,113 @@ const Works = () => {
 
 const Projects = () => {
   return (
-      <ProjectsSection className="content projects " id="projects">
-        <ProjectsHeading>Projects</ProjectsHeading>
-        <ProjectsGrid>
-          {ProjectList.map((list, index) => (
-            <ProjectCard
-              className="project-card"
-              key={index}
-              whileHover={{ scale: 1.05 }}>
-              <CardLeft>
-                <img src={list.img} alt={list.title} />
-              </CardLeft>
-              <CardRight>
-                <h4 className="project-title">{list.title}</h4>
-                <p className="project-description">{list.description}</p>
-                <Stack>
-                  <span className="project-tech-stack">Tech Stack -</span>
-                  {list.tech_stack.map((el, index) => (
-                    <Tag key={index} className="tags">
-                      <img src={el} alt="tech Stacks" />
-                    </Tag>
-                  ))}
-                </Stack>
-                <BtnGroup>
-                  <a
-                    className="project-github-link btn"
-                    href={list.github_url}
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    Github
-                  </a>
-                  <a
-                    className="project-deployed-link btn"
-                    href={list.demo_url}
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    Live
-                  </a>
-                </BtnGroup>
-              </CardRight>
-            </ProjectCard>
-          ))}
-        </ProjectsGrid>
-        <br />
-        <br />
-      </ProjectsSection>
-    );
-  };
-  
-  export default Projects;
-  
-  const typscrpt =
-    "https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg";
-  const ProjectList = [
-    {
-      id: 1,
-      img: chakra,
-      title: "WE Wear (ASOS.COM clone)",
-      description:
-        "Discover fashion redefined at WE Wear, your ultimate destination for premium clothing and timeless style. Drawing inspiration from the acclaimed ASOS.com, we curate collections that epitomize elegance and embrace the essence of true fashion. Unveil your style potential today with WE Wear, where every garment tells a unique story and every choice is a step towards sartorial excellence",
-      tech_stack: [html, css3, javascript],
-      github_url: "https://github.com/Biswajit2595/impartial-honey-6054",
-      demo_url: "https://stunning-frangipane-07bc9c.netlify.app/",
-    },
-    {
-      id: 2,
-      img: chakra,
-      title: "Your Doc",
-      description:
-        "Meet YourDoc: Your go-to web app for hassle-free doctor's appointments. Simplifying healthcare scheduling, YourDoc puts you in control. Quickly find and book appointments with your chosen doctors, focusing on convenience and personalized care. Experience a new era of streamlined healthcare management with YourDoc.",
-      tech_stack: [html, css3, javascript, react, chakra],
-      github_url: "https://github.com/Biswajit2595/earthy-smell-701",
-      demo_url: "https://radiant-sprinkles-ed694a.netlify.app/",
-    },
-    {
-      id: 3,
-      img: chakra,
-      title: "StayUpdate",
-      description:
-        "Discover StayUpdate, your gateway to the latest news and current affairs. Immerse yourself in a world of up-to-date information and engaging articles. With a user-friendly interface, StayUpdate offers seamless access to a wealth of knowledge. Explore diverse topics, stay informed, and indulge your curiosity. StayUpdate – where staying updated is effortless and enlightening.",
-      tech_stack: [html, css3, javascript],
-      github_url: "https://github.com/Biswajit2595/auspicious-oven-3200",
-      demo_url: "https://starlit-basbousa-e2dce5.netlify.app/",
-    },
-    {
-      id: 4,
-      img: chakra,
-      title: "UPSTYLE",
-      description:
-        "Elevate your wardrobe with premium clothing and timeless fashion. Discover curated collections that embody elegance and embrace the essence of style. Unleash your fashion potential today!",
-      tech_stack: [react, redux, chakra, typscrpt],
-      github_url: "https://github.com/Biswajit2595/warlike-current-5989",
-      demo_url: "https://upstylenew.netlify.app/",
-    },
-    {
-      id: 5,
-      img: chakra,
-      title: "GardenEase",
-      description:
-        "Welcome to our vibrant online community dedicated to all things gardening! Our Garden Enthusiast's Haven is a place where green thumbs, budding gardeners, and outdoor enthusiasts come together to share their passion for cultivating nature's beauty.",
-      tech_stack: [react, redux, chakra, javascript, nodejs, mongo, express],
-      github_url: "https://github.com/Biswajit2595/nippy-flavor-9468",
-      demo_url: "https://gardenease.vercel.app/",
-    },
-  ];
+    <ProjectsSection className="content projects " id="projects">
+      <ProjectsHeading>Projects</ProjectsHeading>
+      <ProjectsGrid>
+        {ProjectList.map((list, index) => (
+          <ProjectCard
+            className="project-card"
+            key={index}
+            whileHover={{ scale: 1.05 }}>
+            <CardLeft>
+              <img src={list.img} alt={list.title} />
+            </CardLeft>
+            <CardRight>
+              <h4 className="project-title">{list.title}</h4>
+              <p className="project-description">{list.description}</p>
+              <Stack>
+                <span className="project-tech-stack">Tech Stack -</span>
+                {list.tech_stack.map((el, index) => (
+                  <Tag key={index} className="tags">
+                    <img src={el} alt="tech Stacks" />
+                  </Tag>
+                ))}
+              </Stack>
+              <BtnGroup>
+                <a
+                  className="project-github-link btn"
+                  href={list.github_url}
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  Github
+                </a>
+                <a
+                  className="project-deployed-link btn"
+                  href={list.demo_url}
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  Live
+                </a>
+              </BtnGroup>
+            </CardRight>
+          </ProjectCard>
+        ))}
+      </ProjectsGrid>
+      <br />
+      <br />
+    </ProjectsSection>
+  );
+};
+
+export default Projects;
+
+const typscrpt =
+  "https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg";
+
+const ProjectList = [
+  {
+    id: 1,
+    img: fastFashion,
+    title: "Fast-Fashion",
+    description:
+      "The Fast-Fashion E-Commerce Store project is a collaborative effort by a team of five talented individuals dedicated to providing a dynamic and streamlined online shopping experience in the realm of fashion retail.",
+    tech_stack: [html, css3, javascript, firebase,stripe,react,redux,],
+    github_url: "https://github.com/Abhijit0Barman/Fast-Fashion",
+    demo_url: "https://truculent-face-1803-nine.vercel.app/",
+  },
+  {
+    id: 2,
+    img: travelo,
+    title: "Travelo",
+    description:
+      "Project for an online platform dedicated to promoting sustainable and responsible tourism.",
+    tech_stack: [html, css3, javascript, react, chakra],
+    github_url: "https://github.com/Abhijit0Barman/Travelo",
+    demo_url: "https://vogue-pocket-8479-sooty.vercel.app/",
+  },
+  /* 
+  {
+    id: 3,
+    img: chakra,
+    title: "StayUpdate",
+    description:
+      "Discover StayUpdate, your gateway to the latest news and current affairs. Immerse yourself in a world of up-to-date information and engaging articles. With a user-friendly interface, StayUpdate offers seamless access to a wealth of knowledge. Explore diverse topics, stay informed, and indulge your curiosity. StayUpdate – where staying updated is effortless and enlightening.",
+    tech_stack: [html, css3, javascript],
+    github_url: "https://github.com/Biswajit2595/auspicious-oven-3200",
+    demo_url: "https://starlit-basbousa-e2dce5.netlify.app/",
+  },
+ {
+    id: 4,
+    img: chakra,
+    title: "UPSTYLE",
+    description:
+      "Elevate your wardrobe with premium clothing and timeless fashion. Discover curated collections that embody elegance and embrace the essence of style. Unleash your fashion potential today!",
+    tech_stack: [react, redux, chakra, typscrpt],
+    github_url: "https://github.com/Biswajit2595/warlike-current-5989",
+    demo_url: "https://upstylenew.netlify.app/",
+  },
+  {
+    id: 5,
+    img: chakra,
+    title: "GardenEase",
+    description:
+      "Welcome to our vibrant online community dedicated to all things gardening! Our Garden Enthusiast's Haven is a place where green thumbs, budding gardeners, and outdoor enthusiasts come together to share their passion for cultivating nature's beauty.",
+    tech_stack: [react, redux, chakra, javascript, nodejs, mongo, express],
+    github_url: "https://github.com/Biswajit2595/nippy-flavor-9468",
+    demo_url: "https://gardenease.vercel.app/",
+  },
+  */
+];
 
 // ==========================STYLED-COMPONENT========================
 const ProjectsSection = styled.div`
@@ -181,7 +187,7 @@ const ProjectsGrid = styled.div`
 `;
 
 const ProjectCard = styled(motion.div)`
-  width: 90%;
+  width: 95%;
   margin: auto;
   padding: 1.5rem;
   background-color: beige;
@@ -190,7 +196,7 @@ const ProjectCard = styled(motion.div)`
   display: flex;
   flex-direction: column;
   height: 100%; /* Added a fixed height for uniformity */
-`;
+  `;
 
 const CardLeft = styled.div`
   flex: 1;
@@ -202,9 +208,11 @@ const CardLeft = styled.div`
   justify-content: center;
   align-items: center;
   border-radius: 20px;
+  /* border: 1px solid red; */
+  /* overflow: hidden; */
   img {
     border-radius: 20px;
-    max-height: 100%;
+    max-height: 70%;
     transition: transform 0.5s ease-in-out;
   }
   img:hover {
